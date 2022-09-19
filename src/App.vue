d74fe0be6179f2331fb4ffcef9d85fce7feaeddb
wzl <2540804923@qq.com>
2022-09-19 15:39:54 +0800

    完成初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- state -->
+    <p>{{ $store.state.username }}</p>
+    <!-- getters -->
+    <p>{{ $store.getters.newName }}</p>
+    <button @click="fn">改名</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    console.log(store.state.username)
+    console.log(store.getters.newName);
+    const fn = () => {
+      store.commit('updateName')
+    }
+    return { fn }
+  }
+}
+</script>
+
+<style>
+</style>
